0cda2bbaf8cf2a426d91bd28ddff6dba87c8e332
Drew Vogel <dvogel@github>
2020-12-11 08:46:28 -0600

    Day 10 part 2

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -33,9 +33,32 @@ fn delta_histo(joltages: &Vec<u32>) -> Vec<u32> {
     return histo;
 }
 
+fn count_solutions(joltages: &Vec<u32>) -> u64 {
+    let mut paths: Vec<u64> = vec![1; joltages.len()];
+
+    for i in (0..joltages.len() - 1).rev() {
+        let mut from_here = 0;
+        for j in 1..4 {
+            if i + j < joltages.len() {
+                if joltages[i + j] <= joltages[i] + 3 {
+                    from_here = from_here + paths[i + j];
+                }
+            }
+        }
+        paths[i] = from_here;
+    }
+
+    let mut final_sum = 0;
+    for j in 0..3 {
+        if joltages[j] <= 3 {
+            final_sum = final_sum + paths[j];
+        }
+    }
+    return final_sum;
+}
+
 fn main() {
     let joltages = slurp_input("input");
-    let expected_seq = vec![1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, 22];
     let (valid, seq) = find_seq(&joltages);
     assert_eq!(valid, true);
     let histo: Vec<u32> = delta_histo(&seq);
@@ -43,7 +66,9 @@ fn main() {
         println!("{}: {}", idx + 1, n);
     }
     println!("{} * {} = {}", histo[0], histo[2], histo[0] * histo[2]);
-    return;
+
+    let solution_count: u64 = count_solutions(&seq);
+    println!("{} solutions", solution_count);
 }
 
 #[cfg(test)]
@@ -61,6 +86,9 @@ mod tests {
         assert_eq!(histo[0], 7);
         assert_eq!(histo[1], 0);
         assert_eq!(histo[2], 5);
+
+        let cnt = count_solutions(&seq);
+        assert_eq!(cnt, 8);
     }
 
     #[test]
@@ -72,5 +100,8 @@ mod tests {
         assert_eq!(histo[0], 22);
         assert_eq!(histo[1], 0);
         assert_eq!(histo[2], 10);
+
+        let cnt = count_solutions(&seq);
+        assert_eq!(cnt, 19208);
     }
 }
